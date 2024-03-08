0a08107aa579806da91448d2963eb39d4489d172
The4thMusketeer <kwakuanor18@gmail.com>
2024-03-08 09:32:32 +0000

    added login & logout

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), #defining the 'home' route and the view to be called
+    #path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout')
 ]
